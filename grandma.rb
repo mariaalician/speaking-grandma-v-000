ce27998ee437a657760e01c3ba7309ac9b8c528e
Maria Nunez <mariaalician@gmail.com>
2018-06-10 18:47:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,10 @@
 # Write a speak_to_grandma method.
+phrase = "Hi grandma"
+def speak_to_grandma(phrase)
+  if phrase == 
 
+
+  
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
