f1e1a73c988743df90550e5a1d958994b1ca2f4b
Maria Nunez <mariaalician@gmail.com>
2018-06-10 19:00:02 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma(phrase)
   elsif phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif
+  else
     puts "HUH?! SPEAK UP, SONNY!"
     return "HUH?! SPEAK UP, SONNY!"
   end
