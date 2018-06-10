7b0d89c9bd3305c2cc80f4c1a7b03c3af02e94ba
Maria Nunez <mariaalician@gmail.com>
2018-06-10 18:56:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,8 +6,10 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
   elsif phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
   
