8c719df4d212b2b7f7cbed9ac1486d310ee34158
Maria Nunez <mariaalician@gmail.com>
2018-06-10 18:55:40 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,13 +3,12 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  elsif phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
 end
-
   
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
